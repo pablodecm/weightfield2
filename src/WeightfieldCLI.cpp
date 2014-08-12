b517d5fa25cd7d4c856034a35b3ea4114696e8cb
pablodecm <pablodecm@gmail.com>
2014-08-12 18:35:40 +0200

    Basic display_usage added

--- src/WeightfieldCLI.cpp
+++ src/WeightfieldCLI.cpp
@@ -14,6 +14,7 @@
 
 using namespace std;
 void saveHistsToFile(TString fname, WeightfieldClass* weightF );
+void display_usage();
 
 int main(int argc, char **argv)
 {
@@ -44,10 +45,11 @@ int main(int argc, char **argv)
         {"biasVolt", required_argument, 0, 'v'},
         {"depVolt", required_argument, 0, 'd'},
         {"fname", required_argument, 0, 'o'},
+        {"help", no_argument, 0, 'h'}
       };
       int option_index = 0;
 
-      c = getopt_long (argc, argv, "x:y:p:w:s:b:v:d:o:",long_options, &option_index);
+      c = getopt_long (argc, argv, "x:y:p:w:s:b:v:d:o:h",long_options, &option_index);
 
       /* Detect the end of the options. */
       if (c == -1)
@@ -82,8 +84,11 @@ int main(int argc, char **argv)
         case 'o':
           fname = TString(optarg);
           break;
+        case 'h': /*Intentional fall-through */
         case '?':
           /* getopt_long already printed an error message. */
+          display_usage();
+          return 0;
           break;
         default:
            abort ();
@@ -118,4 +123,19 @@ void saveHistsToFile(TString fname, WeightfieldClass* weightF ){
   histWFieldX->Write();
   histWFieldY->Write();
   fout->Close();
+}
+
+void display_usage()
+{
+  printf("weighfield3-cli usage: \n");
+  printf("  --XMAX or -x : number of strips to simulate \n");
+  printf("  --YMAX or -y : height of the detector (mum)  \n");
+  printf("  --pitch or -p : strip pitch (mum) \n");
+  printf("  --width or -w : strip width (mum) \n");
+  printf("  --stripDop or -s : strip doping type 1=p and 0=n \n");
+  printf("  --bulkDop or -b : bulk doping type 1=p and 0=n  \n");
+  printf("  --biasVolt or -v : bias voltage (V) \n");
+  printf("  --depVolt or -d : depletion voltage(V) \n");
+  printf("  --fname or -o : root output file name \n");
+  printf("  --help or -h : display usage \n");
 }
